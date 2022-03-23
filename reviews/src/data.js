8be6f759cb1bf81cd48baa757cab322ceb8eb91d
LisaVC <lisaviju@gmail.com>
2022-03-23 12:04:41 +0530

    final changes

--- reviews/src/data.js
+++ reviews/src/data.js
@@ -4,7 +4,7 @@ const reviews = [
         name: 'Vishruth S',
         job: 'web developer',
         image:
-            'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+            'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
         text:
             "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
     },
@@ -13,7 +13,7 @@ const reviews = [
         name: 'Lisa VC',
         job: 'web designer',
         image:
-            'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
+            'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
         text:
             'Helvetica artisan kinfolk thundercats lumber blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
     },
@@ -22,7 +22,7 @@ const reviews = [
         name: 'Ananya Daniel',
         job: 'intern',
         image:
-            'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+            'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
         text:
             'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
     },
